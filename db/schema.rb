687aa2f960c217e27d7ee55c32789b1f41ef126c
ryoma7118 <ryoma7118@gmail.com>
2020-09-21 13:18:09 +0900

    ユーザー新規登録機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_18_100410) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "password", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "email", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
