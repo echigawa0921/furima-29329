f06fe8eb533241fca13e8cae2325745f23b0c323
ryoma7118 <ryoma7118@gmail.com>
2020-09-23 16:27:34 +0900

    ユーザー管理機能の実装　修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,7 @@ class User < ApplicationRecord
 
 
   validates :nickname,:birthday, presence: true
-  validates :last_name,:first_name, presence: true,
-                 format: {
-                   with: /\A[ぁ-んァ-ン一-龥]/,
-                 }
-                 validates :last_name_kana,:first_name_kana, presence: true,
-                 format: {
-                   with: /\A[ァ-ヶー－]+\z/,
-                 }
+  validates :last_name,:first_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/,}
+  validates :last_name_kana,:first_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/,}
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
