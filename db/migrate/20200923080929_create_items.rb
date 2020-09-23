f140ac582d294d22e29d82e08174e2e31cb5cf49
ryoma7118 <ryoma7118@gmail.com>
2020-09-23 17:21:22 +0900

    itemモデル・テーブル設計

---
+++ db/migrate/20200923080929_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,               foreign_key: true
+      t.string     :product_name,       null: false
+      t.string     :introduction,       null: false
+      t.integer    :category_id,        null: false
+      t.integer    :status_id,          null: false
+      t.integer    :delivery_fee_id,    null: false
+      t.integer    :shipment_source_id, null: false
+      t.integer    :shippibg_day_id,    null: false
+      t.integer    :price,              null: false 
+      t.timestamps
+    end
+  end
+end
